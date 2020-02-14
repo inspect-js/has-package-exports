b0d76c142232cbeff8a2d88d0497247564016f4f
Jordan Harband <ljharb@gmail.com>
2020-02-13 17:51:53 -0800

    [Tests] add a test for the experimental warning

--- test/index.js
+++ test/index.js
@@ -3,9 +3,25 @@
 var test = require('tape');
 var semver = require('semver');
 var hasPackageExports = require('has-package-exports');
+// eslint-disable-next-line global-require
+var spawnSync = typeof window === 'undefined' && require('child_process').spawnSync;
 
 test('has-package-exports', function (t) {
 	var expected = typeof window === 'undefined' ? semver.satisfies(process.version, '>= 13') : null;
 	t.equal(hasPackageExports, expected, 'module exports expected value');
+
+	t.test('experimental warning', { skip: !spawnSync || process.env.RECURSION }, function (st) {
+		st.plan(1);
+
+		var res = spawnSync('node', ['test'], {
+			env: { PATH: process.env.PATH, RECURSION: 'recursion' }
+		});
+		if (semver.satisfies(process.version, '^13.7 || =12.16.0')) {
+			st.ok(String(res.stderr), 'stderr has an experimental warning in it');
+		} else {
+			st.equal(String(res.stderr), '', 'stderr is empty');
+		}
+	});
+
 	t.end();
 });
